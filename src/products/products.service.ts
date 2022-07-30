2af17dc72015f30b8efdb7c30ce95624f6face7f
Gilver Rolando Ordoñez <admi.jmb@gmail.com>
2022-07-30 18:10:01 -0500

    02 | Create resource "products" +dto+entity+create

---
+++ src/products/products.service.ts
@@ -0,0 +1,59 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  // injected in the constructor -> respository for product
+  private readonly logger = new Logger('ProductsService');
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+
+      return product;
+    } catch (err) {
+      this.handleException(err);
+    }
+  }
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+
+  private handleException(err: any) {
+    // console.error(err);
+    if (err.code === '23505') {
+      throw new BadRequestException(err.detail);
+    }
+    this.logger.error(err);
+    throw new InternalServerErrorException(
+      'We apologize for the problems, if the problem persists, please contact the administrator.',
+    );
+  }
+}
