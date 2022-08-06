ce2f3e8890693a0b5b37b726bbb2c2552b30b701
Rolando Oc <admi.jmb@gmail.com>
2022-08-06 11:48:07 -0500

    24 | Serve files in a controlled manner

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,36 +1,53 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 import { fileFilter } from './helpers/fileFilter.helpers';
 import { fileName } from './helpers/fileNamer.helpers';
 
+const configUpload = {
+  fileFilter,
+  // limits: { fileSize: 1000 },
+  storage: diskStorage({
+    destination: './static/products',
+    filename: fileName,
+  }),
+};
+
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
+  @Get('products/:imageName')
+  findProductImage(
+    @Param('imageName') imageName: string,
+    @Res() res: Response,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+
+    return res.status(200).sendFile(path);
+  }
+
   @Post('products')
-  @UseInterceptors(
-    FileInterceptor('file', {
-      fileFilter,
-      // limits: { fileSize: 1000 },
-      storage: diskStorage({
-        destination: './static/products',
-        filename: fileName,
-      }),
-    }),
-  )
+  @UseInterceptors(FileInterceptor('file', configUpload))
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
     if (!file) {
       throw new BadRequestException('Make sure that the file is an image');
     }
+    const nameHost = 'http://localhost:3000/api/v1/files/products';
+    const nameImage = '/' + file.filename;
+    const secureUrlofImage = nameHost + nameImage;
 
-    return { fileName: file.filename };
+    return { secureUrlofImage };
   }
 }
